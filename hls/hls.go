1e64f29bc2f4cf0c2aa9f7b42c457a3357fe5c45
3052 <spen+3052@protonmail.com>
2023-11-19 21:35:09 -0600

    deleted:    blog/2023-11-18/dash.go

--- hls/hls.go
+++ hls/hls.go
@@ -11,6 +11,34 @@ import (
    "unicode"
 )
 
+type Block struct {
+   cipher.Block
+   key []byte
+}
+
+func New_Block(key []byte) (*Block, error) {
+   block, err := aes.NewCipher(key)
+   if err != nil {
+      return nil, err
+   }
+   return &Block{block, key}, nil
+}
+
+func (b Block) Decrypt(text, iv []byte) []byte {
+   cipher.NewCBCDecrypter(b.Block, iv).CryptBlocks(text, text)
+   if len(text) >= 1 {
+      pad := text[len(text)-1]
+      if len(text) >= int(pad) {
+         text = text[:len(text)-int(pad)]
+      }
+   }
+   return text
+}
+
+func (b Block) Decrypt_Key(text []byte) []byte {
+   return b.Decrypt(text, b.key)
+}
+
 func (s Scanner) Segment() (*Segment, error) {
    var seg Segment
    for s.line.Scan() != scanner.EOF {
@@ -166,34 +194,6 @@ type Mixed interface {
 type Master struct {
    Media []Media
    Stream []Stream
-}
-
-type Block struct {
-   cipher.Block
-   key []byte
-}
-
-func New_Block(key []byte) (*Block, error) {
-   block, err := aes.NewCipher(key)
-   if err != nil {
-      return nil, err
-   }
-   return &Block{block, key}, nil
-}
-
-func (b Block) Decrypt(text, iv []byte) []byte {
-   cipher.NewCBCDecrypter(b.Block, iv).CryptBlocks(text, text)
-   if len(text) >= 1 {
-      pad := text[len(text)-1]
-      if len(text) >= int(pad) {
-         text = text[:len(text)-int(pad)]
-      }
-   }
-   return text
-}
-
-func (b Block) Decrypt_Key(text []byte) []byte {
-   return b.Decrypt(text, b.key)
 }
 
 type Scanner struct {
