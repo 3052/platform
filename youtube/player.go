1cd76960ec4c9689aa37ddf1268af760da47794b
3052 <spen+3052@protonmail.com>
2024-08-24 15:44:57 -0500

    +   body, err := json.MarshalIndent(i, "", " ")

--- youtube/player.go
+++ youtube/player.go
@@ -7,6 +7,46 @@ import (
    "time"
 )
 
+func (i *InnerTube) Player(token *AuthToken) (*Player, error) {
+   i.Context.Client.AndroidSdkVersion = 32
+   i.Context.Client.OsVersion = "12"
+   switch i.Context.Client.ClientName {
+   case android:
+      i.ContentCheckOk = true
+      i.Context.Client.ClientVersion = android_version
+      i.RacyCheckOk = true
+   case android_embedded_player:
+      i.Context.Client.ClientVersion = android_version
+   case web:
+      i.Context.Client.ClientVersion = web_version
+   }
+   body, err := json.MarshalIndent(i, "", " ")
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://www.youtube.com/youtubei/v1/player",
+      bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("user-agent", user_agent + i.Context.Client.ClientVersion)
+   if token != nil {
+      req.Header.Set("authorization", "Bearer " + token.AccessToken)
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   play := &Player{}
+   err = json.NewDecoder(resp.Body).Decode(play)
+   if err != nil {
+      return nil, err
+   }
+   return play, nil
+}
 type Player struct {
    Microformat struct {
       PlayerMicroformatRenderer struct {
@@ -43,45 +83,4 @@ func (d *Date) UnmarshalText(text []byte) error {
       return err
    }
    return nil
-}
-
-func (i *InnerTube) Player(token *AuthToken) (*Player, error) {
-   i.Context.Client.AndroidSdkVersion = 32
-   i.Context.Client.OsVersion = "12"
-   switch i.Context.Client.ClientName {
-   case android:
-      i.ContentCheckOk = true
-      i.Context.Client.ClientVersion = android_version
-      i.RacyCheckOk = true
-   case android_embedded_player:
-      i.Context.Client.ClientVersion = android_version
-   case web:
-      i.Context.Client.ClientVersion = web_version
-   }
-   body, err := json.Marshal(i)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://www.youtube.com/youtubei/v1/player",
-      bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("user-agent", user_agent + i.Context.Client.ClientVersion)
-   if token != nil {
-      req.Header.Set("authorization", "Bearer " + token.AccessToken)
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   play := &Player{}
-   err = json.NewDecoder(resp.Body).Decode(play)
-   if err != nil {
-      return nil, err
-   }
-   return play, nil
 }
