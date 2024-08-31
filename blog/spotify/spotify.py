0f129d4a30aade5697436e95ef7962a1bf7693f5
3052 <spen+3052@protonmail.com>
2024-08-31 16:46:15 -0500

    new file:   blog/spotify/spotify.go

--- blog/spotify/spotify.py
+++ blog/spotify/spotify.py
@@ -1,12 +1,9 @@
-from mitmproxy import ctx, http, tls
-import base64
-import logging
+from mitmproxy import ctx, http
 
 second = False
 
 def request(flow: http.HTTPFlow) -> None:
    global second
-   logging.info(flow.request.path)
    if flow.request.path == '/v3/login':
       if second:
          flow.kill()
@@ -16,7 +13,3 @@ def request(flow: http.HTTPFlow) -> None:
 def running():
    ctx.options.anticomp = True
    ctx.options.showhost = True
-
-def tls_clienthello(data: tls.ClientHelloData):
-   hello = data.client_hello.raw_bytes()
-   logging.info(base64.b64encode(hello))
