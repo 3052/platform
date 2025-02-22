d4d8625c966e5df3955e470cb2665b580ce60cc5
10317 <10317@proton.me>
2025-02-22 08:07:54 -0600

    +func Connect(location string) error {

--- mullvad/mullvad.go
+++ mullvad/mullvad.go
@@ -11,51 +11,6 @@ import (
    "time"
 )
 
-var ErrTimeout = errors.New("timeout")
-
-func Connect(location string) error {
-   data, err := command(
-      "mullvad", "relay", "set", "location", location,
-   ).CombinedOutput()
-   if err != nil {
-      return errors.New(string(data))
-   }
-   err = command("mullvad", "connect").Run()
-   if err != nil {
-      return err
-   }
-   return status("Connected")
-}
-
-func Disconnect() error {
-   err := command("mullvad", "disconnect").Run()
-   if err != nil {
-      return err
-   }
-   return status("Disconnected")
-}
-
-func status(prefix string) error {
-   after := time.After(30 * time.Second)
-   for {
-      select {
-      case <-time.After(time.Second):
-         data, err := command("mullvad", "status").Output()
-         if err != nil {
-            return err
-         }
-         data1 := string(data)
-         if strings.HasPrefix(data1, prefix) {
-            if strings.Contains(data1, " IPv4:") {
-               return nil
-            }
-         }
-      case <-after:
-         return ErrTimeout
-      }
-   }
-}
-
 func command(name string, arg ...string) *exec.Cmd {
    cmd := exec.Command(name, arg...)
    log.Print(cmd.Args)
@@ -106,4 +61,65 @@ func (p *PublicRelays) WireGuard() error {
    }
    defer resp.Body.Close()
    return json.NewDecoder(resp.Body).Decode(p)
+}
+
+var ErrTimeout = errors.New("timeout")
+
+func status(prefix string) error {
+   after := time.After(30 * time.Second)
+   for {
+      select {
+      case <-time.After(time.Second):
+         data, err := command("mullvad", "status").Output()
+         if err != nil {
+            return err
+         }
+         data1 := string(data)
+         if strings.HasPrefix(data1, prefix) {
+            if strings.Contains(data1, " IPv4:") {
+               return nil
+            }
+         }
+      case <-after:
+         return ErrTimeout
+      }
+   }
+}
+
+func Disconnect() error {
+   err := command("mullvad", "disconnect").Run()
+   if err != nil {
+      return err
+   }
+   return status("Disconnected")
+}
+
+func Connect() error {
+   err := command("mullvad", "connect").Run()
+   if err != nil {
+      return err
+   }
+   return status("Connected")
+}
+
+func Relay(location ...string) error {
+   arg := []string{"relay", "set", "location"}
+   arg = append(arg, location...)
+   data, err := command("mullvad", arg...).CombinedOutput()
+   if err != nil {
+      return errors.New(string(data))
+   }
+   return nil
+}
+
+type Transport struct{}
+
+func (Transport) RoundTrip(req *http.Request) (*http.Response, error) {
+   err := Connect()
+   if err != nil {
+      return nil, err
+   }
+   defer Disconnect()
+   log.Println(req.Method, req.URL)
+   return http.DefaultTransport.RoundTrip(req)
 }
--- mullvad/mullvad_test.go
+++ mullvad/mullvad_test.go
@@ -1,42 +1,24 @@
 package mullvad
 
 import (
-   "errors"
    "log"
    "net/http"
    "testing"
 )
 
-var tests = []string{
-   "ca-tor-ovpn-101",
-   "ca-yyc-wg-201",
-}
+// ca-tor-ovpn-101
+// ca-yyc-wg-201
+const paramount = "https://www.paramountplus.com/apps-api/v2.0/androidphone/video/cid/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ.json?at=ABAAAAAAAAAAAAAAAAAAAAAAU%2FEmq2DGAxNGYi71fZdi2mb6UTU2%2BelcD3bGEhs6bo4%3D"
 
 func Test(t *testing.T) {
    log.SetFlags(log.Ltime)
-   for _, test1 := range tests{
-      err := head(test1)
-      if err != nil {
-         t.Fatal(err)
-      }
-   }
-}
-
-const paramount = "https://www.paramountplus.com/apps-api/v2.0/androidphone/video/cid/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ.json?at=ABAAAAAAAAAAAAAAAAAAAAAAU%2FEmq2DGAxNGYi71fZdi2mb6UTU2%2BelcD3bGEhs6bo4%3D"
-
-func head(server string) error {
-   defer Disconnect()
-   err := Connect(server)
+   var client http.Client
+   client.Transport = Transport{}
+   resp, err := client.Head(paramount)
    if err != nil {
-      return err
-   }
-   log.Printf("%v", paramount)
-   resp, err := http.Head(paramount)
-   if err != nil {
-      return err
+      t.Fatal(err)
    }
    if resp.StatusCode != http.StatusOK {
-      return errors.New(resp.Status)
+      t.Fatal(resp.Status)
    }
-   return nil
 }
