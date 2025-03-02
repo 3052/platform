fc163c72f4b79611d502799172bbc55cc21eeb65
10317 <10317@proton.me>
2025-03-02 11:59:04 -0600

    -func Relay(location ...string) error {

--- mullvad/mullvad.go
+++ mullvad/mullvad.go
@@ -9,64 +9,13 @@ import (
    "time"
 )
 
-var ErrTimeout = errors.New("timeout")
-
-func status(prefix string) error {
-   after := time.After(30 * time.Second)
-   for {
-      select {
-      case <-time.After(time.Second):
-         data, err := command("mullvad", "status").Output()
-         if err != nil {
-            return err
-         }
-         data1 := string(data)
-         if strings.HasPrefix(data1, prefix) {
-            if strings.Contains(data1, " IPv4:") {
-               return nil
-            }
-         }
-      case <-after:
-         return ErrTimeout
-      }
-   }
-}
-
-func Relay(location ...string) error {
-   arg := []string{"relay", "set", "location"}
-   arg = append(arg, location...)
-   data, err := command("mullvad", arg...).CombinedOutput()
-   if err != nil {
-      return errors.New(string(data))
-   }
-   return nil
-}
-
 func command(name string, arg ...string) *exec.Cmd {
    cmd := exec.Command(name, arg...)
    log.Print(cmd.Args)
    return cmd
 }
 
-func Disconnect() error {
-   err := command("mullvad", "disconnect").Run()
-   if err != nil {
-      return err
-   }
-   return status("Disconnected")
-}
-
-func Connect() error {
-   err := command("mullvad", "connect").Run()
-   if err != nil {
-      return err
-   }
-   return status("Connected")
-}
-
 type Transport http.Transport
-
-var connected bool
 
 func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
    var err error
@@ -86,4 +35,43 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
       log.Println(req.Method, req.URL)
    }
    return (*http.Transport)(t).RoundTrip(req)
+}
+
+var connected bool
+
+func status(prefix string) error {
+   after := time.After(30 * time.Second)
+   for {
+      select {
+      case <-time.After(time.Second):
+         data, err := command("mullvad", "status").Output()
+         if err != nil {
+            return err
+         }
+         data1 := string(data)
+         if strings.HasPrefix(data1, prefix) {
+            if strings.Contains(data1, " IPv4:") {
+               return nil
+            }
+         }
+      case <-after:
+         return errors.New("timeout")
+      }
+   }
+}
+
+func Connect() error {
+   err := command("mullvad", "connect").Run()
+   if err != nil {
+      return err
+   }
+   return status("Connected")
+}
+
+func Disconnect() error {
+   err := command("mullvad", "disconnect").Run()
+   if err != nil {
+      return err
+   }
+   return status("Disconnected")
 }
