2b47b109e596759bf78588639cb24b4e53c3eaac
10317 <10317@proton.me>
2025-03-02 11:59:04 -0600

    -func Relay(location ...string) error {

--- mullvad/mullvad.go
+++ mullvad/mullvad.go
@@ -1,46 +1,10 @@
 package mullvad
 
 import (
-   "errors"
    "log"
    "net/http"
    "os/exec"
-   "strings"
-   "time"
 )
-
-var ErrTimeout = errors.New("timeout")
-
-func status(prefix string) error {
-   after := time.After(30 * time.Second)
-   for {
-      select {
-      case <-time.After(time.Second):
-         data, err := command("mullvad", "status").Output()
-         if err != nil {
-            return err
-         }
-         data1 := string(data)
-         if strings.HasPrefix(data1, prefix) {
-            if strings.Contains(data1, " IPv4:") {
-               return nil
-            }
-         }
-      case <-after:
-         return ErrTimeout
-      }
-   }
-}
-
-func Relay(location ...string) error {
-   arg := []string{"relay", "set", "location"}
-   arg = append(arg, location...)
-   data, err := command("mullvad", arg...).CombinedOutput()
-   if err != nil {
-      return errors.New(string(data))
-   }
-   return nil
-}
 
 func command(name string, arg ...string) *exec.Cmd {
    cmd := exec.Command(name, arg...)
@@ -48,23 +12,15 @@ func command(name string, arg ...string) *exec.Cmd {
    return cmd
 }
 
-func Disconnect() error {
-   err := command("mullvad", "disconnect").Run()
-   if err != nil {
-      return err
-   }
-   return status("Disconnected")
-}
+type Transport http.Transport
 
 func Connect() error {
-   err := command("mullvad", "connect").Run()
-   if err != nil {
-      return err
-   }
-   return status("Connected")
+   return command("mullvad", "connect", "-w").Run()
 }
 
-type Transport http.Transport
+func Disconnect() error {
+   return command("mullvad", "disconnect", "-w").Run()
+}
 
 var connected bool
 
