facb6daf0cd0e044eccf86eab178f0488534b032
367 <367@tuta.io>
2025-04-12 22:44:40 -0500

    deleted:    nord/nord.json

--- nord/nord.go
+++ nord/nord.go
@@ -6,30 +6,24 @@ import (
    "strconv"
 )
 
-func servers(limit int) ([]server, error) {
-   req, _ := http.NewRequest("", "https://api.nordvpn.com/v1/servers", nil)
+type servers struct {
+   Servers []struct {
+      Hostname string
+      Technologies []struct {
+         Id int
+      }
+   }
+}
+
+func (s *servers) New(limit int) error {
+   req, _ := http.NewRequest("", "https://api.nordvpn.com/v2/servers", nil)
    if limit >= 1 {
       req.URL.RawQuery = "limit=" + strconv.Itoa(limit)
    }
    resp, err := http.DefaultClient.Do(req)
    if err != nil {
-      return nil, err
+      return err
    }
    defer resp.Body.Close()
-   var servers1 []server
-   err = json.NewDecoder(resp.Body).Decode(&servers1)
-   if err != nil {
-      return nil, err
-   }
-   return servers1, nil
-}
-
-type technology struct {
-   Identifier string
-   Name string
-}
-
-type server struct {
-   Hostname string
-   Technologies []technology
+   return json.NewDecoder(resp.Body).Decode(s)
 }
--- nord/nord_test.go
+++ nord/nord_test.go
@@ -5,32 +5,13 @@ import (
    "testing"
 )
 
-func TestSocks(t *testing.T) {
-   servers1, err := servers(9999)
+func Test(t *testing.T) {
+   var servers1 servers
+   err := servers1.New(9999)
    if err != nil {
       t.Fatal(err)
    }
-   for _, server1 := range servers1 {
-      for _, technology1 := range server1.Technologies {
-         if technology1.Identifier == "socks" {
-            fmt.Printf("%+v\n", server1)
-         }
-      }
-   }
-}
-
-func TestTechnology(t *testing.T) {
-   servers1, err := servers(9999)
-   if err != nil {
-      t.Fatal(err)
-   }
-   technologies := map[technology]struct{}{}
-   for _, server1 := range servers1 {
-      for _, technology1 := range server1.Technologies {
-         technologies[technology1] = struct{}{}
-      }
-   }
-   for technology1 := range technologies {
-      fmt.Printf("%+v\n", technology1)
+   for _, server := range servers1.Servers {
+      fmt.Printf("%+v\n", server)
    }
 }
